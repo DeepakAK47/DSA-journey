039b084c979ae4d380629d11eaf5d755fd199539
Deepak Singh <deepaksingh8920984567@gmail.com>
2026-05-14 10:10:34 +0530

    heap_basics

---
+++ Heap/prog1.cpp
@@ -0,0 +1,17 @@
+// basic of priority_queue
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+priority_queue<int>hp;  // It is max_heap by default
+hp.push(10);
+hp.push(20);
+hp.push(30);
+hp.push(-10);
+hp.push(-20);
+hp.push(-30);
+cout<<hp.top()<<endl;
+hp.pop();
+cout<<hp.top()<<endl;
+return 0;
+}
