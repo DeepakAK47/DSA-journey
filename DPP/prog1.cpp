5a6569feac584a02f255886093b46747b5ac80ab
Deepak Singh <deepaksingh8920984567@gmail.com>
2026-07-04 11:40:58 +0530

    find no. of minimum step to reduce n to 0

--- DPP/prog1.cpp
+++ DPP/prog1.cpp
@@ -9,7 +9,7 @@ int f(int n){
     int x = INT_MAX;
     if(n==1) return 0;
     if(n==2 || n==3 ) return 1;
-    return 1+min({f(n-1),(n%2==0) ? f(n/2):x,(n%3==0) ? f(n/3):x}); 
+    return 1+min({f(n-1),(n%2==0) ? f(n/2):x,(n%3==0) ? f(n/3):x});  // for finding the minimum value more than two two digits use {}
 }
 int main(){
     int n;
---
+++ DPP/prog4.cpp
@@ -0,0 +1,34 @@
+// Find the number of minimum ways to reduce n to 0(recursion approach)
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+using namespace std;
+vector<int>digit(int n){
+    vector<int>answer;
+    while(n>0){
+        int d = n%10;
+        if(d!=0) answer.push_back(d);
+        n = n/10;
+    }
+    return answer;
+}
+// f(n)
+int f(int n){
+    if(n==0) return 0;
+    if(n>=1 && n<=9) return 1;
+        // extracting all the ele of the vector
+        vector<int>ele = digit(n);
+        int val = INT_MAX;
+        for(int i=0;i<ele.size();i++){
+            val = min(val,f(n-ele[i]));
+        }
+    return 1 + val; // IMP
+}
+int main(){
+int n;
+cout<<"Enter the value of n : ";
+cin>>n;
+int ans = f(n);
+cout<<ans;
+}
